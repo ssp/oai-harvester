70fffc52ce41b21a7ff1b3c77be982da4bab32e7
ssp <porst@sub.uni-goettingen.de>
2012-02-07 14:01:09 +0100

    harvesting improvements

--- update-oai.py
+++ update-oai.py
@@ -22,13 +22,13 @@ def main():
 	configurationPath = u'config/config.js'
 	formats = False
 	delete = None
-	harvest = False
+	harvest = None
 	transform = False
 	solrXSL = None
 	solrURL = None
 	try:
 		# evaluate command line parameters
-		options, arguments = getopt.getopt(sys.argv[1:], 'c:d:fD:hts:', ['config=', 'datapath=', 'formats', 'delete=', 'harvest', 'transform', 'solr='])
+		options, arguments = getopt.getopt(sys.argv[1:], 'c:d:fD:h:ts:', ['config=', 'datapath=', 'formats', 'delete=', 'harvest=', 'transform', 'solr='])
 		for option, value in options:
 			if option in ('-c', '--config'):
 				configurationPath = value
@@ -39,7 +39,7 @@ def main():
 			elif option in ('-f', '--formats'):
 				formats = True
 			elif option in ('-h', '--harvest'):
-				harvest = True
+				harvest = value
 			elif option in ('-t', '--transform'):
 				transform = True
 			elif option in ('-s', '--solr'):
@@ -57,13 +57,13 @@ def main():
 		if delete != None:
 			deleteFiles(delete)
 
-		# Read transformation XSL if needed
+		# Read transformation XSL if needed.
 		if transform != None:
 			try:
 				solrXSLXML = etree.parse('OAI-to-Solr.xsl')
 				solrXSL = etree.XSLT(solrXSLXML)
 			except:
-				printerror('Failed to read XSL for Solr transformation.')
+				printError('Failed to read XSL for Solr transformation.')
 
 		# loop through repositories and run actions determined by the command line parameters
 		for repositoryID in sorted(repositories.iterkeys()):
@@ -72,13 +72,13 @@ def main():
 			
 			if not repository.has_key('broken'):
 				print ''
-				print u'==== ' + repositoryID + u' ===='
+				printHeading(u'==== ' + repositoryID + u' ====', repository)
 				
 				if formats:
 					determineFormats(repository)
 				
-				if harvest:
-					updateOAI(repository, configuration)
+				if harvest != None:
+					updateOAI(repository, configuration, harvest.split(','))
 
 				if transform:
 					transformXML(repository, solrXSL)
@@ -93,10 +93,10 @@ def main():
 				solrCommit = urllib2.urlopen(updateURL, "commit=true")
 				solrCommit.close()
 			except urllib2.URLError as err:
-				printerror('Failed to commit the Solr index: ' + str(err))
+				printError('Failed to commit the Solr index: ' + str(err))
 		
 	except getopt.GetoptError, err:
-		printerror('Could not parse the options: ' + str(err))
+		printError('Could not parse the options: ' + str(err))
 		sys.exit(2)
 
 
@@ -121,7 +121,7 @@ def deleteData (dataType):
 
 
 def determineFormats (repository):
-	printheader (u'Supported formats:')
+	printHeading (u'Supported formats:')
 		
 	formatsXML = runOAIRequest(repository, 'ListMetadataFormats', timeout = 10)
 	if formatsXML != None:
@@ -133,7 +133,7 @@ def determineFormats (repository):
 			if prefixElement != None and prefixElement != None:
 				prefix = prefixElement.text
 			else:
-				printerror('Metadata format without prefix: ' + str(prefixElement))
+				printError('Metadata format without prefix: ' + str(prefixElement))
 
 			namespace = ''
 			namespaceElement = format.find(OAI2Namespace + 'metadataNamespace')
@@ -142,86 +142,103 @@ def determineFormats (repository):
 
 			print repository['ID'] + '\t' + prefix + '\t' + namespace
 	else:
-		printerror('Could not list metadata formats.', repository)
+		printError('Could not list metadata formats.', repository)
 
 
 
-def updateOAI (repository, configuration):
-	print bcolors + u'Harvest OAI data'
-	
-	# Find the latest commited download and extract its responseDate.
-	lastResponseDate = None
-	repositoryOAIPath = repositoryPath('oai', repository)
-	fileList = os.listdir(repositoryOAIPath).sort()
-	if fileList != None and len(fileList) > 0:
-		newestFile = fileList[-1]
-		f = open(newestFile)
-		newestFileText = f.read()
-		f.close()
-		XML = xml.etree.ElementTree.fromstring(newestFileText)
-		responseDateElement = XML.find('responseDate')
-		if responseDateElement != None:
-			lastResponseDate = responseDateElement.text
-			print u'Last response date: ' + lastResponseDate
-
+def updateOAI (repository, configuration, collections):
 	# Use existing sets or create a single placeholder one if we don’t need them.
 	sets = {None: None}
 	if repository.has_key('sets'):
 		sets = repository['sets']
 	# Loop over sets.
 	for setID in sets:
-		fileCount = 0
 		set = sets[setID]
-		repositoryOAITempPath = repositoryPath('oai-temp', repository, setID, True)
-		
-		# Configure and run initial OAI request.
-		options = {'metadataPrefix': configuration['format']}
-		if repository.has_key('format'):
-			options['metadataPrefix'] = repository['format']
-		if lastResponseDate != None:
-			options['from'] = lastResponseDate
-		if setID != '':
-			options['set'] = set
-		recordsXML = runOAIRequest(repository, 'ListRecords', options)
-		
-		# Loop until no more data comes from the server.
-		while recordsXML != None:
-			# Store XML to temp folder.
-			fileCount = fileCount + 1
-			XMLString = xml.etree.ElementTree.tostring(recordsXML, encoding='utf-8')
-			path = repositoryOAITempPath + '/' + '%09d'%(fileCount) + '.xml'
-			XMLFile = open(path, 'w')
-			XMLFile.write(XMLString)
-			XMLFile.close()
-			print u'Wrote file »' + path + u'«'
-		
-			listRecordsElement = recordsXML.find(OAI2Namespace + 'ListRecords')
-			if listRecordsElement != None:
-				tokenElement = recordsXML.find(OAI2Namespace + 'ListRecords' + '/' + OAI2Namespace + 'resumptionToken')
-				# Extract resumption token.
-				if tokenElement != None and tokenElement.text != None:
-					options = {'resumptionToken': tokenElement.text}
-					recordsXML = runOAIRequest(repository, 'ListRecords', options)
-				else:
+
+		# Do we need to harvest this set?
+		needToHarvest = (collections == ['all'])
+		if not needToHarvest:
+			folderName = repository['ID']
+			if setID != None:
+				folderName = folderName + u'_' + setID
+			for collection in collections:
+				if folderName in configuration['collections'][collection]:
+					needToHarvest = True
 					break
+							
+		if needToHarvest:
+			heading = u'Harvest OAI data'
+			if setID != None:
+				heading = heading + ' for set ' + setID + ' (' + set + ')'
+			printHeading(heading)
+
+			# Find the latest commited download and extract its responseDate.
+			lastResponseDate = None
+			repositoryOAIPath = repositoryPath('oai', repository, setID)
+			fileList = os.listdir(repositoryOAIPath)
+			fileList.sort()
+			if fileList != None and len(fileList) > 0:
+				newestFile = repositoryOAIPath + '/' + fileList[-1]
+				print newestFile 
+				f = open(newestFile)
+				newestFileText = f.read()
+				f.close()
+				XML = xml.etree.ElementTree.fromstring(newestFileText)
+				responseDateElement = XML.find(OAI2Namespace + 'responseDate')
+				if responseDateElement != None and responseDateElement.text != None:
+					lastResponseDate = responseDateElement.text.split('T')[0]
+					print u'Last response date: ' + lastResponseDate
 					
-				# See whether there are any records in the download.
-				# If there aren’t, stop downloading (to deal with repositories
-				# which violate the specification and return a non-empty resumption token
-				# when there are no results)
-				recordElements = listRecordsElement.findall(OAI2Namespace + 'record')
-				if len(recordElements) == 0:
-					printerror('Empty list of records: stopping', repository)
-					break
+			# Configure and run initial OAI request.
+			options = {'metadataPrefix': configuration['format']}
+			if repository.has_key('format'):
+				options['metadataPrefix'] = repository['format']
+			if lastResponseDate != None:
+				options['from'] = lastResponseDate
+			if setID != None:
+				options['set'] = set
+			recordsXML = runOAIRequest(repository, 'ListRecords', options)
+			
+			# Loop until no more data comes from the server.
+			repositoryOAITempPath = repositoryPath('oai-temp', repository, setID, True)
+			fileCount = 0
+			while recordsXML != None:
+				# Store XML to temp folder.
+				fileCount = fileCount + 1
+				XMLString = xml.etree.ElementTree.tostring(recordsXML, encoding='utf-8')
+				path = repositoryOAITempPath + '/' + '%09d'%(fileCount) + '.xml'
+				XMLFile = open(path, 'w')
+				XMLFile.write(XMLString)
+				XMLFile.close()
+				print u'Wrote file »' + path + u'«'
+			
+				listRecordsElement = recordsXML.find(OAI2Namespace + 'ListRecords')
+				if listRecordsElement != None:
+					tokenElement = recordsXML.find(OAI2Namespace + 'ListRecords' + '/' + OAI2Namespace + 'resumptionToken')
+					# Extract resumption token.
+					if tokenElement != None and tokenElement.text != None:
+						options = {'resumptionToken': tokenElement.text}
+						recordsXML = runOAIRequest(repository, 'ListRecords', options)
+					else:
+						break
+						
+					# See whether there are any records in the download.
+					# If there aren’t, stop downloading (to deal with repositories
+					# which violate the specification and return a non-empty resumption token
+					# when there are no results)
+					recordElements = listRecordsElement.findall(OAI2Namespace + 'record')
+					if len(recordElements) == 0:
+						printError('Empty list of records: stopping', repository)
+						break
+					
+				else:
+					printError('No »ListRecords« element in the download: stopping', repository)
+					
+				if fileCount > maximumChunks:
+					printError(u'Downloaded more than ' + maximumChunks + u': stopping.', repository)
 				
 			else:
-				printerror('No »ListRecords« element in the download: stopping', repository)
-				
-			if fileCount > maximumChunks:
-				printerror(u'Downloaded more than ' + maximumChunks + u': stopping.', repository)
-			
-		else:
-			printerror('Could not list records.', repository)
+				printError('Could not list records.', repository)
 	
 
 
@@ -241,11 +258,11 @@ def runOAIRequest (repository, verb, parameters = {}, timeout = 30):
 				errorMessage = errorElement.attrib['code'] + u' ' + errorMessage
 			if errorElement.text != None:
 				errorMessage = errorMessage + errorElement.text
-			printerror(errorMessage, repository)
+			printError(errorMessage, repository)
 			XML = None
 		
 	except xml.etree.ElementTree.ParseError as err:
-		printerror(u'Could not parse XML: ' + str(err), repository)
+		printError(u'Could not parse XML: ' + str(err), repository)
 	
 	return XML
 	
@@ -261,19 +278,19 @@ def getURL (URL, timeout = 30):
 		connection.close()
 	
 	except httplib.InvalidURL as err:
-		printerror(u'Invalid URL »' + URL + u'«: ' + str(err))
+		printError(u'Invalid URL »' + URL + u'«: ' + str(err))
 	except urllib2.URLError as err:
-		printerror(u'Could not load URL ' + str(err))
+		printError(u'Could not load URL ' + str(err))
 	except socket.timeout as err:
-		printerror(u'Gave up after ' + str(timeout) + u' second timeout.')
+		printError(u'Gave up after ' + str(timeout) + u' second timeout.')
 	
 	return result
 
 
 	
 
-def transformXML (repository):
-	printheader('Transforming OAI records to Solr records')
+def transformXML (repository, XSL):
+	printHeading('Transforming OAI records to Solr records')
 	# Run through XML files in oai-temp folder
 	OAITempPath = repositoryPath('oai-temp', repository)
 	OAIPath = repositoryPath('oai', repository)
@@ -283,25 +300,24 @@ def transformXML (repository):
 		if fileList != None:
 			fileList.sort()
 			for fileName in fileList:
-				print fileName
 				solrFilePath = solrTempPath + '/' + fileName
 				OAIFilePath = OAITempPath + '/' + fileName
 				try:
 					fileXML = etree.parse(OAIFilePath)
-					solrXML = xsl(fileXML, collections="'geoleo-oai'")
+					solrXML = XSL(fileXML, collections="'geoleo-oai'")
 					solrFile = open(solrFilePath, 'w')
 					solrFile.write(etree.tostring(solrXML, encoding='utf-8', method='xml'))
 					solrFile.close()
-					print "Created Solr file »" + solrFilePath + "«"
+					print u'Created Solr file »' + solrFilePath + u'«'
 					moveFile(fileName, OAITempPath, OAIPath)
 					
 				except:
-					printerror("Could not convert file »" + fileName + "«", repository)
+					printError(u'Could not convert file »' + fileName + u'«', repository)
 
 
 
 def updateSolr (repository, solrURL):
-	printheading('Uploading to Solr')
+	printHeading('Uploading to Solr')
 	
 	repositorySolrTempPath = repositoryPath('solr-temp', repository)
 	repositorySolrPath = repositoryPath('solr', repository)
@@ -352,22 +368,22 @@ def repositoryPath (dataType, repository = None, dataSet = None, emptyFolder = F
 		elif emptyFolder == True:
 			if os.listdir(path) != None:
 				if len(os.listdir(path)) > 0:
-					print u'Removing ' + str(len(os.listdir(path))) + u' files from »' + path + u'«'
+					print u'Removing ' + str(len(os.listdir(path))) + u' file(s) from »' + path + u'«'
 					shutil.rmtree(path)
 					os.makedirs(path)
 	else:
-		printerror(u'Need a dataType to determine the repositoryPath.', repository)
+		printError(u'Need a dataType to determine the repositoryPath.', repository)
 
 	return path
 
 
 
-def printHeader (message, repository):
+def printHeading (message, repository = None):
 	print bcolors.HEADER + message + bcolors.ENDC
 
 
 
-def printerror (message, repository = None):
+def printError (message, repository = None):
 	output = bcolors.FAIL + u'ERROR: ' + bcolors.ENDC
 	if repository != None:
 		output = output + u'(' + repository['ID'] + u') '
